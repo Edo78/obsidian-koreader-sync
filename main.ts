511ac281fc586cf517743f29c5e31edc17c3cdf9
Federico Granata <3602209+Edo78@users.noreply.github.com>
2022-01-19 22:17:12 +0100

    An ugly and barely working PoC

--- main.ts
+++ main.ts
@@ -1,85 +1,78 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, addIcon } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface KOReaderSettings {
+	koreaderBasePath: string;
+  obsidianNoteFolder: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: KOReaderSettings = {
+	koreaderBasePath: '/media/user/KOBOeReader',
+  obsidianNoteFolder: '/'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+interface Bookmark {
+	chapter: string;
+	text: string;
+	datetime: string;
+	notes: string;
+	highlighted: boolean;
+	pos0: string;
+	pos1?: string;
+	page: string;
+}
+
+interface Bookmarks {
+	[key: number]: Bookmark;
+}
+
+interface Book {
+	title: string;
+	authors: string;
+	bookmarks: Bookmarks;
+	highlight: any;
+}
+
+interface Books {
+	[fullTitle: string]: Book;
+}
+
+export default class KOReader extends Plugin {
+	settings: KOReaderSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		const ribbonIconEl = this.addRibbonIcon('documents', 'KOReader Plugin', async (evt: MouseEvent) => {
+			const metadata = new KOReaderMetadata(this.settings.koreaderBasePath);
+			const data: Books = await metadata.scan();
+			// console.log(JSON.stringify(data, null, 2));
+			
+			const existingFiles = this.app.vault.getMarkdownFiles().map(file => file.path);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+			for (const book in data) {
+				for (const bookmark in data[book].bookmarks) {
+          const note = data[book].bookmarks[bookmark];
+          const noteTitle = note.text.split(data[book].bookmarks[bookmark].datetime)[1].replace(/^\s+|\s+$/g, '');
+					const body = `# Title: [[${data[book].title} - ${data[book].authors}|${data[book].title}]]
+by: [[${data[book].authors}]]
+## Chapter: ${note.chapter}
+**==${note.notes}==**
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+${noteTitle}
+`;
+					const path = this.settings.obsidianNoteFolder === '/' ? '' : this.settings.obsidianNoteFolder + '/';
+					const notePath = `${path}${noteTitle} (${bookmark}).md`;
+					if (!existingFiles.includes(notePath)) {
+						this.app.vault.create(notePath, body);
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new KoreaderSettingTab(this.app, this));
 	}
 
 	onunload() {
-
 	}
 
 	async loadSettings() {
@@ -91,26 +84,55 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class KOReaderMetadata {
+	koreaderBasePath: string;
+	FindFiles = require("node-find-files");
+	path = require('path');
+	luaJson = require('lua-json')
+	fs = require('fs');
+
+	constructor(koreaderBasePath: string) {
+		this.koreaderBasePath = koreaderBasePath;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	public async scan(): Promise<Books> {
+		const metadatas:any = {};
+		return new Promise((resolve, reject) => {
+			const finder = new this.FindFiles({
+				rootFolder: this.koreaderBasePath,
+			});
+			finder.on('match', (file: string) => {
+				const filename = this.path.parse(file).base;
+				if (filename.match(/metadata\..*\.lua$/)) {
+					const content = this.fs.readFileSync(file, 'utf8');
+					const jsonMetadata = this.luaJson.parse(content);
+					const { highlight, bookmarks, doc_props: { title }, doc_props: { authors } } = jsonMetadata;
+					if (Object.keys(highlight).length && Object.keys(bookmarks).length) {
+						metadatas[`${title} - ${authors}`] = {
+							title,
+							authors,
+							// highlight,
+							bookmarks,
+						};
+					}
+				}
+			});
+			finder.on('error', (err: any) => {
+				console.log(err);
+				reject(err);
+			});
+			finder.on('complete', () => {
+				resolve(metadatas);
+			});
+			finder.startSearch();
+		});
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class KoreaderSettingTab extends PluginSettingTab {
+	plugin: KOReader;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: KOReader) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,18 +142,38 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'KOReader plugin settings'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('KOReader mounted path')
+			.setDesc('/media/<user>/KOBOeReader')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter the path wher KOReader is mounted')
+				.setValue(this.plugin.settings.koreaderBasePath)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					console.log('Path: ' + value);
+					this.plugin.settings.koreaderBasePath = value;
 					await this.plugin.saveSettings();
 				}));
-	}
+
+		this.app.vault.getAbstractFileByPath
+    new Setting(containerEl)
+      .setName('Highlights folder location')
+      .setDesc('Vault folder to use for writing book highlight notes')
+      .addDropdown((dropdown) => {
+
+        const files = (this.app.vault.adapter as any).files;
+				const folders = Object.keys(files).filter(key => files[key].type === 'folder');
+				console.log(folders);
+				folders.forEach((val) => {
+					dropdown.addOption(val, val);
+				});
+        return dropdown
+					.setValue(this.plugin.settings.obsidianNoteFolder) 
+					.onChange(async (value) => {
+						this.plugin.settings.obsidianNoteFolder = value;
+            await this.plugin.saveSettings();
+          });
+      });
+  }
 }
