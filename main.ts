e09d5d4363974c6b793c0dceb64acd1385d20d6a
Federico "Edo" Granata <3602209+Edo78@users.noreply.github.com>
2022-01-19 08:56:46 +0100

    initial PoC

--- main.ts
+++ main.ts
@@ -2,80 +2,199 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface KOReaderSettings {
+	basePath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: KOReaderSettings = {
+	basePath: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+// datetime is a string like "2022-01-18 19:14:05"
+interface Bookmark {
+	chapter: string;
+	text: string;
+	datetime: string;
+	notes: string;
+	highlighted: boolean;
+	pos0: string;
+	pos1?: string;
+	page: string;
+}
+
+interface Bookmarks {
+	[key: number]: Bookmark;
+}
+
+const test: Bookmarks = {
+	1: {
+		chapter: '1',
+		text: 'test',
+		datetime: '2020-01-18 19:14:05',
+		notes: 'questa è la prima nota'
+	},
+	2: {
+		chapter: '2',
+		text: 'test 2',
+		datetime: '2020-01-18 19:14:05',
+		notes: 'questa è la seconda nota'
+	}
+};
+
+interface Book {
+	title: string;
+	authors: string;
+	bookmarks: Bookmarks;
+	highlight: any;
+}
+
+interface Books {
+	[fullTitle: string]: Book;
+}
+
+const books: Books = {
+  "Altre storie straordinarie delle materie prime - Alessandro Giraudo": {
+    "title": "Altre storie straordinarie delle materie prime",
+    "authors": "Alessandro Giraudo",
+    "highlight": {
+      "73": {
+        "1": {
+          "drawer": "lighten",
+          "pos0": "/body/DocFragment[28]/body/p[2]/text().402",
+          "pos1": "/body/DocFragment[28]/body/p[2]/text().421",
+          "text": "mamelucchi circassi",
+          "chapter": "Il sapone: una ricetta dei Sumeri, prodotto a Venezia e a Marsiglia",
+          "datetime": "2022-01-18 19:14:05"
+        }
+      }
+    },
+    "bookmarks": {
+      "1": {
+        "highlighted": true,
+        "chapter": "Il sapone: una ricetta dei Sumeri, prodotto a Venezia e a Marsiglia",
+        "text": "Pagina 73 mamelucchi circassi @ 2022-01-18 19:14:05\n\nSe non ricordo male mia madre usava il termine mammalucco",
+        "pos0": "/body/DocFragment[28]/body/p[2]/text().402",
+        "pos1": "/body/DocFragment[28]/body/p[2]/text().421",
+        "page": "/body/DocFragment[28]/body/p[2]/text().402",
+        "notes": "mamelucchi circassi",
+        "datetime": "2022-01-18 19:14:05"
+      }
+    }
+  },
+  "Tutti i romanzi e i racconti - Howard Phillips Lovecraft": {
+    "title": "Tutti i romanzi e i racconti",
+    "authors": "Howard Phillips Lovecraft",
+    "highlight": {
+      "748": {
+        "1": {
+          "datetime": "2022-01-16 18:47:41",
+          "chapter": "L’orrore a Red Hook*",
+          "pos0": "/body/DocFragment[36]/body/p[201]/i/text().0",
+          "drawer": "lighten",
+          "text": "An sint unquam daemones incubi et succubae, et an ex tali congressu proles enasci queat?7",
+          "pos1": "/body/DocFragment[36]/body/p[201]/sup/a/text().1"
+        }
+      },
+      "754": {
+        "1": {
+          "datetime": "2022-01-16 18:52:27",
+          "chapter": "L’orrore a Red Hook*",
+          "pos0": "/body/DocFragment[36]/body/p[229]/i[1]/text().0",
+          "drawer": "lighten",
+          "text": "Encyclopaedia of Occultism di Lewis Spence",
+          "pos1": "/body/DocFragment[36]/body/p[229]/text()[2].16"
+        }
+      },
+      "895": {
+        "1": {
+          "datetime": "2022-01-18 15:01:20",
+          "chapter": "L’ultimo esperimento di Clarendon*",
+          "pos0": "/body/DocFragment[43]/body/p[18]/text().293",
+          "drawer": "lighten",
+          "text": "gran parte le malattie che si diffondono sul globo scaturiscono dalle zone sconosciute dell’antichissima e misteriosa Asia.",
+          "pos1": "/body/DocFragment[43]/body/p[18]/text().417"
+        }
+      }
+    },
+    "bookmarks": {
+      "1": {
+        "datetime": "2022-01-18 15:01:20",
+        "highlighted": true,
+        "notes": "gran parte le malattie che si diffondono sul globo scaturiscono dalle zone sconosciute dell’antichissima e misteriosa Asia.",
+        "pos0": "/body/DocFragment[43]/body/p[18]/text().293",
+        "pos1": "/body/DocFragment[43]/body/p[18]/text().417",
+        "page": "/body/DocFragment[43]/body/p[18]/text().293",
+        "chapter": "L’ultimo esperimento di Clarendon*",
+        "text": "Pagina 895 gran parte le malattie che si diffondono sul globo scaturiscono dalle zone sconosciute dell’antichissima e misteriosa Asia. @ 2022-01-18 15:01:20\n\nCome il covid?"
+      },
+      "2": {
+        "datetime": "2022-01-16 18:52:27",
+        "highlighted": true,
+        "notes": "Encyclopaedia of Occultism di Lewis Spence",
+        "pos0": "/body/DocFragment[36]/body/p[229]/i[1]/text().0",
+        "pos1": "/body/DocFragment[36]/body/p[229]/text()[2].16",
+        "text": "Pagina 754 Encyclopaedia of Occultism di Lewis Spence @ 2022-01-16 18:52:27\n\nEnciclopedia da cui spesso Lovecraft prendeva spunto",
+        "chapter": "L’orrore a Red Hook*",
+        "page": "/body/DocFragment[36]/body/p[229]/i[1]/text().0"
+      },
+      "3": {
+        "datetime": "2022-01-16 18:47:41",
+        "highlighted": true,
+        "notes": "An sint unquam daemones incubi et succubae, et an ex tali congressu proles enasci queat?7",
+        "pos0": "/body/DocFragment[36]/body/p[201]/i/text().0",
+        "pos1": "/body/DocFragment[36]/body/p[201]/sup/a/text().1",
+        "text": "Pagina 748 An sint unquam daemones incubi et succubae, et an ex tali congressu proles enasci queat?7 @ 2022-01-16 18:47:41\n\nCosa vuol dire?",
+        "chapter": "L’orrore a Red Hook*",
+        "page": "/body/DocFragment[36]/body/p[201]/i/text().0"
+      }
+    }
+  }
+};
+
+export default class KOReader extends Plugin {
+	settings: KOReaderSettings;
 
 	async onload() {
 		await this.loadSettings();
+		const statusBarItemEl = this.addStatusBarItem();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		const ribbonIconEl = this.addRibbonIcon('dice', 'KOReader Plugin', async (evt: MouseEvent) => {
+			// This will scan for metadata
+			const metadata = new KOReaderMetadata(this.settings.basePath, statusBarItemEl);
+			// const data: Books = await metadata.scan();
+			const data: Books = await metadata.fakeScan();
+			console.log(JSON.stringify(data, null, 2));
+			
+			// Loop on each book in 'data' and on each bookmark
+			// then create a note in the vault
+			for (const book in data) {
+				// console.log('book', data[book]);
+				for (const bookmark in data[book].bookmarks) {
+					// const note = new Note(data[book].title, data[book].Bookmarks[bookmark].chapter, data[book].Bookmarks[bookmark].text, data[book].Bookmarks[bookmark].notes);
+					const note = `# Title: [[${data[book].title}]]
+## Authors: [[${data[book].authors}]]
+## Chapter: ${data[book].bookmarks[bookmark].chapter}
+**==${data[book].bookmarks[bookmark].notes}==**
+
+${data[book].bookmarks[bookmark].text}
+`;
+					this.app.vault.create(`${data[book].title} - ${data[book].authors}.md`, note);
+				}
+			}
+
+			// this.app.vault.create('KOReader.md', 'questa è una prova');
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -91,26 +210,167 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class KOReaderMetadata {
+	basePath: string;
+	FindFiles = require("node-find-files");
+	statusBarItemEl: HTMLElement;
+	path = require('path');
+	luaJson = require('lua-json')
+	fs = require('fs');
+
+	constructor(basePath: string, statusBarItemEl: HTMLElement) {
+		this.basePath = basePath;
+		this.statusBarItemEl = statusBarItemEl;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	public async fakeScan(): Promise<Books> {
+		return Promise.resolve({
+  "Altre storie straordinarie delle materie prime - Alessandro Giraudo": {
+    "title": "Altre storie straordinarie delle materie prime",
+    "authors": "Alessandro Giraudo",
+    "highlight": {
+      "73": {
+        "1": {
+          "drawer": "lighten",
+          "pos0": "/body/DocFragment[28]/body/p[2]/text().402",
+          "pos1": "/body/DocFragment[28]/body/p[2]/text().421",
+          "text": "mamelucchi circassi",
+          "chapter": "Il sapone: una ricetta dei Sumeri, prodotto a Venezia e a Marsiglia",
+          "datetime": "2022-01-18 19:14:05"
+        }
+      }
+    },
+    "bookmarks": {
+      "1": {
+        "highlighted": true,
+        "chapter": "Il sapone: una ricetta dei Sumeri, prodotto a Venezia e a Marsiglia",
+        "text": "Pagina 73 mamelucchi circassi @ 2022-01-18 19:14:05\n\nSe non ricordo male mia madre usava il termine mammalucco",
+        "pos0": "/body/DocFragment[28]/body/p[2]/text().402",
+        "pos1": "/body/DocFragment[28]/body/p[2]/text().421",
+        "page": "/body/DocFragment[28]/body/p[2]/text().402",
+        "notes": "mamelucchi circassi",
+        "datetime": "2022-01-18 19:14:05"
+      }
+    }
+  },
+  "Tutti i romanzi e i racconti - Howard Phillips Lovecraft": {
+    "title": "Tutti i romanzi e i racconti",
+    "authors": "Howard Phillips Lovecraft",
+    "highlight": {
+      "748": {
+        "1": {
+          "datetime": "2022-01-16 18:47:41",
+          "chapter": "L’orrore a Red Hook*",
+          "pos0": "/body/DocFragment[36]/body/p[201]/i/text().0",
+          "drawer": "lighten",
+          "text": "An sint unquam daemones incubi et succubae, et an ex tali congressu proles enasci queat?7",
+          "pos1": "/body/DocFragment[36]/body/p[201]/sup/a/text().1"
+        }
+      },
+      "754": {
+        "1": {
+          "datetime": "2022-01-16 18:52:27",
+          "chapter": "L’orrore a Red Hook*",
+          "pos0": "/body/DocFragment[36]/body/p[229]/i[1]/text().0",
+          "drawer": "lighten",
+          "text": "Encyclopaedia of Occultism di Lewis Spence",
+          "pos1": "/body/DocFragment[36]/body/p[229]/text()[2].16"
+        }
+      },
+      "895": {
+        "1": {
+          "datetime": "2022-01-18 15:01:20",
+          "chapter": "L’ultimo esperimento di Clarendon*",
+          "pos0": "/body/DocFragment[43]/body/p[18]/text().293",
+          "drawer": "lighten",
+          "text": "gran parte le malattie che si diffondono sul globo scaturiscono dalle zone sconosciute dell’antichissima e misteriosa Asia.",
+          "pos1": "/body/DocFragment[43]/body/p[18]/text().417"
+        }
+      }
+    },
+    "bookmarks": {
+      "1": {
+        "datetime": "2022-01-18 15:01:20",
+        "highlighted": true,
+        "notes": "gran parte le malattie che si diffondono sul globo scaturiscono dalle zone sconosciute dell’antichissima e misteriosa Asia.",
+        "pos0": "/body/DocFragment[43]/body/p[18]/text().293",
+        "pos1": "/body/DocFragment[43]/body/p[18]/text().417",
+        "page": "/body/DocFragment[43]/body/p[18]/text().293",
+        "chapter": "L’ultimo esperimento di Clarendon*",
+        "text": "Pagina 895 gran parte le malattie che si diffondono sul globo scaturiscono dalle zone sconosciute dell’antichissima e misteriosa Asia. @ 2022-01-18 15:01:20\n\nCome il covid?"
+      },
+      "2": {
+        "datetime": "2022-01-16 18:52:27",
+        "highlighted": true,
+        "notes": "Encyclopaedia of Occultism di Lewis Spence",
+        "pos0": "/body/DocFragment[36]/body/p[229]/i[1]/text().0",
+        "pos1": "/body/DocFragment[36]/body/p[229]/text()[2].16",
+        "text": "Pagina 754 Encyclopaedia of Occultism di Lewis Spence @ 2022-01-16 18:52:27\n\nEnciclopedia da cui spesso Lovecraft prendeva spunto",
+        "chapter": "L’orrore a Red Hook*",
+        "page": "/body/DocFragment[36]/body/p[229]/i[1]/text().0"
+      },
+      "3": {
+        "datetime": "2022-01-16 18:47:41",
+        "highlighted": true,
+        "notes": "An sint unquam daemones incubi et succubae, et an ex tali congressu proles enasci queat?7",
+        "pos0": "/body/DocFragment[36]/body/p[201]/i/text().0",
+        "pos1": "/body/DocFragment[36]/body/p[201]/sup/a/text().1",
+        "text": "Pagina 748 An sint unquam daemones incubi et succubae, et an ex tali congressu proles enasci queat?7 @ 2022-01-16 18:47:41\n\nCosa vuol dire?",
+        "chapter": "L’orrore a Red Hook*",
+        "page": "/body/DocFragment[36]/body/p[201]/i/text().0"
+      }
+    }
+  }
+});
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	// a public method will scan the basePath for metadata files
+	public async scan(): Promise<Books> {
+		const metadatas:any = {};
+		return new Promise((resolve, reject) => {
+			const finder = new this.FindFiles({
+				rootFolder: this.basePath,
+			});
+			finder.on('match', (file: string) => {
+				const filename = this.path.parse(file).base;
+				if (filename.match(/metadata\..*\.lua$/)) {
+					// read the file
+					const content = this.fs.readFileSync(file, 'utf8');
+					// console.log(file);
+					const jsonMetadata = this.luaJson.parse(content);
+					// console.log(JSON.stringify(jsonMetadata, null, 2));
+					const { highlight, bookmarks, doc_props: { title }, doc_props: { authors } } = jsonMetadata;
+					// parse only if highlight and bookmarks are present
+					// console.log('test', (Object.keys(highlight).length && Object.keys(bookmarks).length) ? true : false);
+					if (Object.keys(highlight).length && Object.keys(bookmarks).length) {
+						// console.log(`${filename} - ${title} - ${authors}`);
+						metadatas[`${title} - ${authors}`] = {
+							title,
+							authors,
+							highlight,
+							bookmarks,
+						};
+						// console.log(JSON.stringify(metadatas, null, 2));
+					}
+				}
+			});
+			finder.on('error', (err: any) => {
+				console.log(err);
+				reject(err);
+			});
+			finder.on('complete', () => {
+				// console.log(JSON.stringify(metadatas, null, 2));
+				resolve(metadatas);
+			});
+			finder.startSearch();
+		});
 	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: KOReader;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: KOReader) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,17 +380,17 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'KOReader plugin settings'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('KOReader mounted path')
+			.setDesc('/mnt/user/onboard')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter the path to KOReader')
+				.setValue(this.plugin.settings.basePath)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					console.log('Path: ' + value);
+					this.plugin.settings.basePath = value;
 					await this.plugin.saveSettings();
 				}));
 	}
