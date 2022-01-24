df0726ecc426cf1320e3e91e14bc9efc5602fb44
Federico Granata <3602209+Edo78@users.noreply.github.com>
2022-01-24 16:36:29 +0100

    fix unacceptable chars in note name

--- src/main.ts
+++ src/main.ts
@@ -1,107 +1,151 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, addIcon } from 'obsidian';
-import { KOReaderMetadata } from './koreader-metadata';
+import { App, Plugin, PluginSettingTab, Setting, normalizePath } from 'obsidian';
+
 import { Books } from './types';
+import { KOReaderMetadata } from './koreader-metadata';
 
 interface KOReaderSettings {
-	koreaderBasePath: string;
+  koreaderBasePath: string;
   obsidianNoteFolder: string;
 }
 
 const DEFAULT_SETTINGS: KOReaderSettings = {
-	koreaderBasePath: '/media/user/KOBOeReader',
-  obsidianNoteFolder: '/'
+  koreaderBasePath: '/media/user/KOBOeReader',
+  obsidianNoteFolder: '/',
+};
+
+interface TitleOptions {
+  prefix?: string;
+  suffix?: string;
+  maxLength?: number;
+  maxWords?: number;
 }
 
 export default class KOReader extends Plugin {
-	settings: KOReaderSettings;
+  settings: KOReaderSettings;
 
-	async onload() {
-		await this.loadSettings();
+  private manageTitle(title: string, options: TitleOptions = {}): string {
+    // replace \ / and : with _
+    title = title.replace(/\\|\/|:/g, '_');
+    // replace multiple underscores with one underscore
+    title = title.replace(/_+/g, '_');
+    // remove leading and trailing whitespace
+    title = title.trim();
+    // remove leading and trailing underscores
+    title = title.replace(/^_+|_+$/g, '');
+    // replace multiple spaces with one space
+    title = title.replace(/\s+/g, ' ');
+    // if options.maxLength is set, trim the title to that length and add '...'
+    if (options.maxLength && title.length > options.maxLength) {
+      title = title.substring(0, options.maxLength) + '...';
+    }
+    // if options.maxWords is set, trim the title to that number of words and add '...'
+    if (options.maxWords && title.split(' ').length > options.maxWords) {
+      title = title.split(' ').slice(0, options.maxWords).join(' ') + '...';
+    }
 
-		const ribbonIconEl = this.addRibbonIcon('documents', 'KOReader Plugin', async (evt: MouseEvent) => {
-			const metadata = new KOReaderMetadata(this.settings.koreaderBasePath);
-			const data: Books = await metadata.scan();
-			// console.log(JSON.stringify(data, null, 2));
-			
-			const existingFiles = this.app.vault.getMarkdownFiles().map(file => file.path);
+    return `${options.prefix || ''}${title}${options.suffix || ''}`;
+  }
 
-			for (const book in data) {
-				for (const bookmark in data[book].bookmarks) {
-          const note = data[book].bookmarks[bookmark];
-          const noteTitle = note.text.split(data[book].bookmarks[bookmark].datetime)[1].replace(/^\s+|\s+$/g, '');
-					const body = `# Title: [[${data[book].title} - ${data[book].authors}|${data[book].title}]]
+  async onload() {
+    await this.loadSettings();
+
+    const ribbonIconEl = this.addRibbonIcon(
+      'documents',
+      'KOReader Plugin',
+      async (evt: MouseEvent) => {
+        const metadata = new KOReaderMetadata(this.settings.koreaderBasePath);
+        const data: Books = await metadata.scan();
+        // console.log(JSON.stringify(data, null, 2));
+
+        const existingFiles = this.app.vault.getMarkdownFiles().map((file) => file.path);
+
+        for (const book in data) {
+          for (const bookmark in data[book].bookmarks) {
+            const note = data[book].bookmarks[bookmark];
+            const noteItself = note.text ? note.text
+              .split(data[book].bookmarks[bookmark].datetime)[1]
+              .replace(/^\s+|\s+$/g, '') : '';
+            const noteTitle = noteItself ?
+              this.manageTitle(noteItself) :
+              `${this.manageTitle(data[book].title)} - ${data[book].authors}`;
+
+            const body = `# Title: [[${this.manageTitle(data[book].title)} - ${data[book].authors}|${data[book].title}]]
 by: [[${data[book].authors}]]
 ## Chapter: ${note.chapter}
 **==${note.notes}==**
 
-${noteTitle}
+${noteItself}
 `;
-					const path = this.settings.obsidianNoteFolder === '/' ? '' : this.settings.obsidianNoteFolder + '/';
-					const notePath = `${path}${noteTitle} (${bookmark}).md`;
-					if (!existingFiles.includes(notePath)) {
-						this.app.vault.create(notePath, body);
-					}
-				}
-			}
-		});
+            const path =
+              this.settings.obsidianNoteFolder === '/'
+                ? ''
+                : `${this.settings.obsidianNoteFolder}/`;
+            const notePath = normalizePath(`${path}${noteTitle} (${bookmark}).md`);
+            if (!existingFiles.includes(notePath)) {
+              this.app.vault.create(notePath, body);
+            }
+          }
+        }
+      }
+    );
 
-		this.addSettingTab(new KoreaderSettingTab(this.app, this));
-	}
+    this.addSettingTab(new KoreaderSettingTab(this.app, this));
+  }
 
-	onunload() {
-	}
+  onunload() { }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  async loadSettings() {
+    this.settings = { ...DEFAULT_SETTINGS, ...(await this.loadData()) };
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
 class KoreaderSettingTab extends PluginSettingTab {
-	plugin: KOReader;
+  plugin: KOReader;
 
-	constructor(app: App, plugin: KOReader) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: KOReader) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const {containerEl} = this;
+  display(): void {
+    const { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'KOReader plugin settings'});
+    containerEl.createEl('h2', { text: 'KOReader plugin settings' });
 
-		new Setting(containerEl)
-			.setName('KOReader mounted path')
-			.setDesc('/media/<user>/KOBOeReader')
-			.addText(text => text
-				.setPlaceholder('Enter the path wher KOReader is mounted')
-				.setValue(this.plugin.settings.koreaderBasePath)
-				.onChange(async (value) => {
-					console.log('Path: ' + value);
-					this.plugin.settings.koreaderBasePath = value;
-					await this.plugin.saveSettings();
-				}));
+    new Setting(containerEl)
+      .setName('KOReader mounted path')
+      .setDesc('/media/<user>/KOBOeReader')
+      .addText((text) =>
+        text
+          .setPlaceholder('Enter the path wher KOReader is mounted')
+          .setValue(this.plugin.settings.koreaderBasePath)
+          .onChange(async (value) => {
+            console.log(`Path: ${value}`);
+            this.plugin.settings.koreaderBasePath = value;
+            await this.plugin.saveSettings();
+          })
+      );
 
     new Setting(containerEl)
       .setName('Highlights folder location')
       .setDesc('Vault folder to use for writing book highlight notes')
       .addDropdown((dropdown) => {
-
-        const files = (this.app.vault.adapter as any).files;
-				const folders = Object.keys(files).filter(key => files[key].type === 'folder');
-				console.log(folders);
-				folders.forEach((val) => {
-					dropdown.addOption(val, val);
-				});
+        const { files } = this.app.vault.adapter as any;
+        const folders = Object.keys(files).filter((key) => files[key].type === 'folder');
+        console.log(folders);
+        folders.forEach((val) => {
+          dropdown.addOption(val, val);
+        });
         return dropdown
-					.setValue(this.plugin.settings.obsidianNoteFolder) 
-					.onChange(async (value) => {
-						this.plugin.settings.obsidianNoteFolder = value;
+          .setValue(this.plugin.settings.obsidianNoteFolder)
+          .onChange(async (value) => {
+            this.plugin.settings.obsidianNoteFolder = value;
             await this.plugin.saveSettings();
           });
       });
