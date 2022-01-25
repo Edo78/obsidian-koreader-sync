debe2bbc6093347acde370730f144e5383e7e7a7
Federico "Edo" Granata <3602209+Edo78@users.noreply.github.com>
2022-01-25 16:24:49 +0100

    chore: refactor

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-import { App, Plugin, PluginSettingTab, Setting, normalizePath } from 'obsidian';
+import {
+  App,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+  normalizePath,
+} from 'obsidian';
 import * as matter from 'gray-matter';
 import * as crypto from 'crypto';
-import { Books } from './types';
+import { Book, Bookmark, Books } from './types';
 import { KOReaderMetadata } from './koreader-metadata';
 
 interface KOReaderSettings {
@@ -46,11 +52,11 @@ export default class KOReader extends Plugin {
     title = title.replace(/\s+/g, ' ');
     // if options.maxLength is set, trim the title to that length and add '...'
     if (options.maxLength && title.length > options.maxLength) {
-      title = title.substring(0, options.maxLength) + '...';
+      title = `${title.substring(0, options.maxLength)}...`;
     }
     // if options.maxWords is set, trim the title to that number of words and add '...'
     if (options.maxWords && title.split(' ').length > options.maxWords) {
-      title = title.split(' ').slice(0, options.maxWords).join(' ') + '...';
+      title = `${title.split(' ').slice(0, options.maxWords).join(' ')}...`;
     }
 
     return `${options.prefix || ''}${title}${options.suffix || ''}`;
@@ -62,50 +68,7 @@ export default class KOReader extends Plugin {
     const ribbonIconEl = this.addRibbonIcon(
       'documents',
       'KOReader Plugin',
-      async (evt: MouseEvent) => {
-        const metadata = new KOReaderMetadata(this.settings.koreaderBasePath);
-        const data: Books = await metadata.scan();
-
-        const existingNotes = this.app.vault.getMarkdownFiles().map((f) => {
-          const fm = this.app.metadataCache.getFileCache(f)?.frontmatter;
-          return (fm?.['koreader-sync']?.['uniqueId'])
-        });
-
-        for (const book in data) {
-          for (const bookmark in data[book].bookmarks) {
-            const uniqueId = crypto.createHash('md5').update(`${data[book].title} - ${data[book].authors} - ${data[book].bookmarks[bookmark].pos0} - ${data[book].bookmarks[bookmark].pos1}`).digest("hex");
-            if (existingNotes.includes(uniqueId)) {
-              continue;
-            }
-            const path =
-              this.settings.obsidianNoteFolder === '/'
-                ? ''
-                : `${this.settings.obsidianNoteFolder}/`;
-            const note = data[book].bookmarks[bookmark];
-            const noteItself = note.text ? note.text
-              .split(data[book].bookmarks[bookmark].datetime)[1]
-              .replace(/^\s+|\s+$/g, '') : '';
-            const noteTitle = noteItself ?
-              this.manageTitle(noteItself, this.settings.noteTitleOptions) :
-              `${this.manageTitle(note.notes, this.settings.noteTitleOptions)} - ${data[book].authors}`;
-
-            const frontmatterData = {
-              'koreader-sync': {
-                'uniqueId': uniqueId,
-              },
-            };
-            const content = `# Title: [[${path}${this.manageTitle(data[book].title, this.settings.bookTitleOptions)} - ${data[book].authors}|${data[book].title}]]
-by: [[${data[book].authors}]]
-## Chapter: ${note.chapter}
-**==${note.notes}==**
-
-${noteItself}
-`;
-            const notePath = normalizePath(`${path}${noteTitle}.md`);
-            this.app.vault.create(notePath, matter.stringify(content, frontmatterData));
-          }
-        }
-      }
+      this.importNotes.bind(this)
     );
 
     this.addSettingTab(new KoreaderSettingTab(this.app, this));
@@ -119,6 +82,82 @@ ${noteItself}
 
   async saveSettings() {
     await this.saveData(this.settings);
+  }
+
+  private createNote(note: {
+    path: string;
+    uniqueId: string;
+    bookmark: Bookmark;
+    managedBookTitle: string;
+    book: Book;
+  }) {
+    const { path, uniqueId, bookmark, managedBookTitle, book } = note;
+    const noteItself = bookmark.text
+      ? bookmark.text.split(bookmark.datetime)[1].replace(/^\s+|\s+$/g, '')
+      : '';
+    const noteTitle = noteItself
+      ? this.manageTitle(noteItself, this.settings.noteTitleOptions)
+      : `${this.manageTitle(
+        bookmark.notes,
+        this.settings.noteTitleOptions
+      )} - ${book.authors}`;
+    const notePath = normalizePath(`${path}/${noteTitle}`);
+    const frontmatterData = {
+      'koreader-sync': {
+        uniqueId,
+      },
+    };
+    const content = `# Title: [[${normalizePath(
+      `${path}/${managedBookTitle}`
+    )}|${book.title}]]
+by: [[${book.authors}]]
+## Chapter: ${bookmark.chapter}
+**==${bookmark.notes}==**
+
+${noteItself}
+`;
+
+    return { content, frontmatterData, notePath };
+  }
+
+  async importNotes(evt: MouseEvent) {
+    const metadata = new KOReaderMetadata(this.settings.koreaderBasePath);
+    const data: Books = await metadata.scan();
+
+    const existingNotes = this.app.vault.getMarkdownFiles().map((f) => {
+      const fm = this.app.metadataCache.getFileCache(f)?.frontmatter;
+      return fm?.['koreader-sync']?.uniqueId;
+    });
+
+    for (const book in data) {
+      const managedBookTitle = `${this.manageTitle(
+        data[book].title,
+        this.settings.bookTitleOptions
+      )}-${data[book].authors}`;
+      for (const bookmark in data[book].bookmarks) {
+        const uniqueId = crypto
+          .createHash('md5')
+          .update(
+            `${data[book].title} - ${data[book].authors} - ${data[book].bookmarks[bookmark].pos0} - ${data[book].bookmarks[bookmark].pos1}`
+          )
+          .digest('hex');
+        if (existingNotes.includes(uniqueId)) {
+          continue;
+        }
+        const { content, frontmatterData, notePath } = this.createNote({
+          path: this.settings.obsidianNoteFolder,
+          uniqueId,
+          bookmark: data[book].bookmarks[bookmark],
+          managedBookTitle,
+          book: data[book],
+        });
+
+        this.app.vault.create(
+          `${notePath}.md`,
+          matter.stringify(content, frontmatterData)
+        );
+      }
+    }
   }
 }
 
@@ -156,8 +195,9 @@ class KoreaderSettingTab extends PluginSettingTab {
       .setDesc('Vault folder to use for writing book highlight notes')
       .addDropdown((dropdown) => {
         const { files } = this.app.vault.adapter as any;
-        const folders = Object.keys(files).filter((key) => files[key].type === 'folder');
-        console.log(folders);
+        const folders = Object.keys(files).filter(
+          (key) => files[key].type === 'folder'
+        );
         folders.forEach((val) => {
           dropdown.addOption(val, val);
         });
